873cceb467bbd2b9b5e4cc6b5ad6bc80d730d838
BOU ALWAN, NADIM <nbou@iberdrola.com>
2024-08-26 09:57:18 +0300

    Fix encodeDecode.py

--- encodeDecode.py
+++ encodeDecode.py
@@ -1,23 +1,32 @@
 def encode(strs: list[str]) -> str:
-    new_str = ""
-    for s in strs:
-        new_str += "[" + s + "]"
-    return new_str
+    delim = "$"
+    return "".join([str(len(s)) + delim + s for s in strs])
 
 
 def decode(s: str) -> list[str]:
     l = []
-    count = 0
-    for i, c in enumerate(s):
-        if c == "[":
-            if count == 0:
-                start = i + 1
-            count += 1
-        elif c == "]":
-            if count == 1:
-                end = i
-                l.append(s[start:end])
-            count -= 1
+    delim = "$"
+    number_started = False
+    i = 0
+    
+    while i < len(s):
+        c = s[i]
+        if c in "0123456789" and not number_started:
+            number_started = True
+            num = c
+        elif c in "0123456789" and number_started:
+            num += c
+        elif c == delim and number_started:
+            number_started = False
+            num = int(num)
+            l.append(s[i+1:i+1+num])
+            i += num
+        i += 1
     return l
 
-# This only works if the brackets are balanced.
+slist = ["we","say",":","yes","!@#$%^&*()"]
+e = encode(slist)
+d = decode(e)
+print(e)
+print(d)
+print (slist == decode(encode(slist)))
