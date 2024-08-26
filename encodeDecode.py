37ab469adeeabc5f8f9d25a8aedec3ed44f795f6
BOU ALWAN, NADIM <nbou@iberdrola.com>
2024-08-26 09:23:05 +0300

    Identify problem with encodeDecode

--- encodeDecode.py
+++ encodeDecode.py
@@ -8,14 +8,16 @@ def encode(strs: list[str]) -> str:
 def decode(s: str) -> list[str]:
     l = []
     count = 0
-    for i in range(len(s)):
-        if s[i] == "[":
+    for i, c in enumerate(s):
+        if c == "[":
             if count == 0:
                 start = i + 1
             count += 1
-        elif s[i] == "]":
+        elif c == "]":
             if count == 1:
                 end = i
                 l.append(s[start:end])
             count -= 1
     return l
+
+# This only works if the brackets are balanced.
